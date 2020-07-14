05c1fe1b91d01abd797174ad2e3784c3599d2c36
shubhamrawat153 <53989778+shubhamrawat153@users.noreply.github.com>
2020-07-14 14:32:31 +0530

    first commit

---
+++ ARTwo/Assets/Data Files/Scripts/resetpos.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resetpos : MonoBehaviour
+{
+    Vector3 originalpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalpos = transform.localPosition;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider collide)
+    {
+        if (collide.gameObject.CompareTag("AEROPLANEWALL"))
+        {
+            transform.localPosition = originalpos;
+        }
+        else if (collide.gameObject.CompareTag("CLOUDWALL"))
+        {
+            transform.localPosition = originalpos;
+        }
+    }
+}
